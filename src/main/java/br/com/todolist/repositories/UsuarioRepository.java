7b6beb2a1f8a3b69e39330a5bcef576f16a098a9
Diogo Evangelista <68126@PROWAY.TREINA>
2020-11-29 16:05:01 -0300

    criando repositorio de usuario

---
+++ src/main/java/br/com/todolist/repositories/UsuarioRepository.java
@@ -0,0 +1,23 @@
+package br.com.todolist.repositories;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.todolist.model.Usuario;
+
+public class UsuarioRepository implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager entityManager;
+	
+	public List<Usuario> listarTudo() {
+		return entityManager.createQuery("from usuario", Usuario.class).getResultList();
+	}
+
+
+}
