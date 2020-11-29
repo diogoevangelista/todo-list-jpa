eb48d19d63156803fe10c4096c2142f2ca36850e
Diogo Evangelista <68126@PROWAY.TREINA>
2020-11-29 15:25:04 -0300

    aprimorando model ListaTarefas

--- src/main/java/br/com/todolist/model/ListaTarefa.java
+++ src/main/java/br/com/todolist/model/ListaTarefa.java
@@ -1,50 +1,107 @@
 package br.com.todolist.model;
 
 import java.io.Serializable;
+import java.sql.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class ListaTarefa implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String nome;
-	
-	public ListaTarefa(long id, String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
-	
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "descricao", nullable = false, length = 120)
+	private String descricao;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_cadastro")
+	private Date dataCadastro;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_execucao")
+	private Date dataExecucao;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_conclusao")
+	private Date dataConclusao;
+
+	@Column(name = "observacao", length = 120)
+	private String observacao;
+
 	public ListaTarefa() {}
 
-	public long getId() {
+	public ListaTarefa(Long id, String descricao, Date dataCadastro, Date dataExecucao, Date dataConclusao,
+			String observacao) {
+		this.id = id;
+		this.descricao = descricao;
+		this.dataCadastro = dataCadastro;
+		this.dataExecucao = dataExecucao;
+		this.dataConclusao = dataConclusao;
+		this.observacao = observacao;
+	}
+
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Date getDataExecucao() {
+		return dataExecucao;
+	}
+
+	public void setDataExecucao(Date dataExecucao) {
+		this.dataExecucao = dataExecucao;
+	}
+
+	public Date getDataConclusao() {
+		return dataConclusao;
+	}
+
+	public void setDataConclusao(Date dataConclusao) {
+		this.dataConclusao = dataConclusao;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -57,10 +114,13 @@ public class ListaTarefa implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		ListaTarefa other = (ListaTarefa) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
-
+	
 	
 }
