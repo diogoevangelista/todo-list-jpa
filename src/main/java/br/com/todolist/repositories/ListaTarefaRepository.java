e1809fb850a66ca0f0370c438198f14793dbddea
Diogo Evangelista <68126@PROWAY.TREINA>
2020-12-06 16:01:37 -0300

    metodo de salvar nova tarefa

--- src/main/java/br/com/todolist/repositories/ListaTarefaRepository.java
+++ src/main/java/br/com/todolist/repositories/ListaTarefaRepository.java
@@ -7,16 +7,22 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import br.com.todolist.model.ListaTarefa;
+import br.com.todolist.util.Transacional;
 
 public class ListaTarefaRepository implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EntityManager entityManager;
-	
+
 	public List<ListaTarefa> listarTudo() {
 		return entityManager.createQuery("from lista_tarefa", ListaTarefa.class).getResultList();
+	}
+
+	@Transacional
+	public ListaTarefa salvar(ListaTarefa novaTarefa) {
+		return entityManager.merge(novaTarefa);
 	}
 
 }
